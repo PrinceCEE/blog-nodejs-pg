30c60ad13439942441d59582745fd0d516287c5c
Chimezie <princecee15@gmail.com>
2023-05-22 10:21:51 +0100

    initial config

---
+++ src/constants/index.ts
@@ -0,0 +1,6 @@
+import { ITableNames } from "src/types";
+
+export const COMMENT_TABLE_NAME: ITableNames = "comments";
+export const POST_TABLE_NAME: ITableNames = "posts";
+export const USER_TABLE_NAME: ITableNames = "users";
+export const AUTH_TABLE_NAME: ITableNames = "auths";
