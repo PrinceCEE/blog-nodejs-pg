a2c1dcc6ac1051c867c09fd0c0632b15e792260e
Chimezie <princecee15@gmail.com>
2023-05-22 10:21:31 +0100

    linked the app and db

---
+++ src/index.ts
@@ -0,0 +1,25 @@
+import dotenv from "dotenv";
+dotenv.config();
+
+import http from "http";
+import app from "./app";
+import { initDB } from "./db";
+
+const startApp = async () => {
+  const PORT = process.env["PORT"];
+  const server = http.createServer(app);
+
+  await initDB();
+  console.log("Server connected to the DB");
+
+  server.listen(PORT, () => {
+    console.log(`Server started on port ${PORT}`);
+  });
+
+  server.on("error", (err) => {
+    console.error(err);
+    process.exit(1);
+  });
+};
+
+startApp();
