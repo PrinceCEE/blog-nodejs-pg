3abaf053b4623cbef37683b8d5ac1134abd865b1
Chimezie <princecee15@gmail.com>
2023-05-22 10:01:26 +0100

    basic middlewares setup

---
+++ src/middlewares/index.ts
@@ -0,0 +1,4 @@
+export { default as AuthMiddleware } from "./auth.middleware";
+export { default as CommentMiddleware } from "./comment.middleware";
+export { default as PostMiddleware } from "./post.middleware";
+export { default as UserMiddleware } from "./user.middleware";
