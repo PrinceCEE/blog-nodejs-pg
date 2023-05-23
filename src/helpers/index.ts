5c4498380d96cead5e8777e7a0e34420a36d9b97
Chimezie <princecee15@gmail.com>
2023-05-23 19:15:32 +0100

    added hashpassword, encryptPassword and errorhandler

---
+++ src/helpers/index.ts
@@ -0,0 +1,49 @@
+import { IAccessToken, IResponse, Next, Req, Res } from "src/types";
+import { compare, genSalt, hash } from "bcrypt";
+import { sign, verify } from "jsonwebtoken";
+import { TOKEN_EXPIRES_IN, TOKEN_SECRET } from "src/constants";
+
+export const errorHandler =
+  (handler: (req: Req, res?: Res) => Promise<IResponse>) =>
+  async (req: Req, res: Res, next: Next) => {
+    try {
+      const response = await handler(req, res);
+      return res.json(response);
+    } catch (err) {
+      return next(err);
+    }
+  };
+
+export const hashPassword = async (password: string) => {
+  const salt = await genSalt();
+  return hash(password, salt);
+};
+
+export const confirmPassword = async (hashedPwd: string, password: string) => {
+  return compare(password, hashedPwd);
+};
+
+export const encryptAccessToken = (data: IAccessToken) => {
+  return new Promise<string>((resolve, reject) => {
+    try {
+      const token = sign(data, TOKEN_SECRET, {
+        expiresIn: TOKEN_EXPIRES_IN,
+      });
+
+      resolve(token);
+    } catch (err) {
+      reject(err);
+    }
+  });
+};
+
+export const decodeAccessToken = (token: string) => {
+  return new Promise<IAccessToken>((resolve, reject) => {
+    try {
+      const data = verify(token, TOKEN_SECRET) as IAccessToken;
+      resolve(data);
+    } catch (err) {
+      reject(err);
+    }
+  });
+};
