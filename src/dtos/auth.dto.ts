f527c641607e03cc468b9548eb2db607e12b6e8f
Chimezie <princecee15@gmail.com>
2023-05-23 19:13:55 +0100

    added auth dtos

---
+++ src/dtos/auth.dto.ts
@@ -0,0 +1,20 @@
+export class RegisterDto {
+  firstName: string;
+  lastName: string;
+  email: string;
+  password: string;
+}
+
+export class LoginDto {
+  email: string;
+  password: string;
+}
+
+export class ForgotPasswordDto {
+  email: string;
+}
+
+export class ChangePasswordDto {
+  password: string;
+  uuid: string;
+}
