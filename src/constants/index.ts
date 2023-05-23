d5351e201c21eae321cec90ee473f1deb99b7c24
Chimezie <princecee15@gmail.com>
2023-05-23 19:11:17 +0100

    added token secret and token expiration

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -4,3 +4,6 @@ export const COMMENT_TABLE_NAME: ITableNames = "comments";
 export const POST_TABLE_NAME: ITableNames = "posts";
 export const USER_TABLE_NAME: ITableNames = "users";
 export const AUTH_TABLE_NAME: ITableNames = "auths";
+
+export const TOKEN_SECRET = "s$cret";
+export const TOKEN_EXPIRES_IN = "7d";
