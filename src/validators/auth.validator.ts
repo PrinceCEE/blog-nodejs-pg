7b3afe2cc29cfe49fe30f14aa35029c349e2e33e
Chimezie <princecee15@gmail.com>
2023-05-23 19:14:33 +0100

    added auth validators

---
+++ src/validators/auth.validator.ts
@@ -0,0 +1,29 @@
+import Joi from "joi";
+import {
+  ChangePasswordDto,
+  ForgotPasswordDto,
+  LoginDto,
+  RegisterDto,
+} from "src/dtos";
+
+const joiStr = Joi.string();
+
+export const RegisterSchema = Joi.object<RegisterDto>({
+  firstName: joiStr.required(),
+  lastName: joiStr.required(),
+  email: joiStr.email().required(),
+  password: joiStr.min(8).required(),
+});
+
+export const LoginSchema = Joi.object<LoginDto>({
+  email: joiStr.email().required(),
+  password: joiStr.required(),
+});
+
+export const ForgotPasswordSchema = Joi.object<ForgotPasswordDto>({
+  email: joiStr.email().required(),
+});
+
+export const ChangePasswordSchema = Joi.object<ChangePasswordDto>({
+  password: joiStr.min(8).required(),
+});
