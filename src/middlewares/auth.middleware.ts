3eeafdc621435db66043a836ff84ab882ff79e1c
Chimezie <princecee15@gmail.com>
2023-05-23 23:48:02 +0100

    added the access token middleware

--- src/middlewares/auth.middleware.ts
+++ src/middlewares/auth.middleware.ts
@@ -1,1 +1,29 @@
-export default class AuthMiddleware {}
+import { getObjects } from "src/db";
+import { NotFoundError, UnauthorizedError } from "src/errors";
+import { decodeAccessToken } from "src/helpers";
+import { Next, Req, Res, User } from "src/types";
+
+export default class AuthMiddleware {
+  verifyAccessToken = async (req: Req, res: Res, next: Next) => {
+    try {
+      const token = req.headers["authorization"].split(" ")[1];
+      if (token) {
+        throw new UnauthorizedError("No token found");
+      }
+
+      const tokenData = await decodeAccessToken(token);
+      const user = await getObjects<User>("users", {
+        userID: tokenData.userID,
+      }).then((rows) => rows[0]);
+
+      if (!user) {
+        throw new NotFoundError("User not found");
+      }
+
+      req.user = user;
+      next();
+    } catch (err) {
+      return next(err);
+    }
+  };
+}
--- src/types/express.types.ts
+++ src/types/express.types.ts
@@ -1,5 +1,6 @@
 import { Request, Response, NextFunction } from "express";
+import { User } from "./table.types";
 
-export type Req = Request;
+export type Req = Request & { user?: User };
 export type Res = Response;
 export type Next = NextFunction;
