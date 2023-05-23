365955877bf7968b4e4109e53d92ecf1d94fb760
Chimezie <princecee15@gmail.com>
2023-05-23 07:53:26 +0100

    added the error handler

--- src/routers/auth.router.ts
+++ src/routers/auth.router.ts
@@ -1,15 +1,16 @@
 import { Router } from "express";
 import { AuthController } from "src/controllers";
+import { errorHandler } from "src/helpers";
 import { AuthMiddleware } from "src/middlewares";
 
 export default () => {
   const authController = new AuthController();
   const router = Router();
 
-  router.post("/register", authController.register);
-  router.post("/login", authController.login);
-  router.post("/forgot-password", authController.forgotPassword);
-  router.post("/change-password", authController.changePassword);
+  router.post("/register", errorHandler(authController.register));
+  router.post("/login", errorHandler(authController.login));
+  router.post("/forgot-password", errorHandler(authController.forgotPassword));
+  router.post("/change-password", errorHandler(authController.changePassword));
 
   return router;
 };
