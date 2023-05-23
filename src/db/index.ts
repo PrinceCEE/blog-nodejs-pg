14667de9230d30c9f1b585e58d4b3dc6cb4d476f
Chimezie <princecee15@gmail.com>
2023-05-23 19:08:15 +0100

    added the helper function for fetching data

--- src/db/index.ts
+++ src/db/index.ts
@@ -4,12 +4,13 @@ import {
   COMMENT_SCHEMA,
   POST_SCHEMA,
   USER_SCHEMA,
+  UUID_EXTENSION,
 } from "./schemas";
 import { ITableNames } from "src/types";
 
 export const pool = new pg.Pool();
 
-export const insertObject = <T>(table: ITableNames, data: any) => {
+export const insertObject = async <T>(table: ITableNames, data: Partial<T>) => {
   const keys = Object.keys(data);
   const values = Object.values(data);
 
@@ -19,13 +20,33 @@ export const insertObject = <T>(table: ITableNames, data: any) => {
 
   const columns = keys.join(", ");
   const placeholders = values.map((_, i) => `$${i + 1}`).join(", ");
-  return pool.query<T[]>(
+  const queryResult = await pool.query<T[]>(
     `INSERT INTO ${table} (${columns}) VALUES (${placeholders}) RETURNING *`,
     values
   );
+
+  return queryResult.rows[0] as T;
 };
 
-export const updateObject = (table: ITableNames, data: any) => {};
+export const getObjects = async <T>(table: ITableNames, filter: Partial<T>) => {
+  const keys = Object.keys(filter);
+  const values = Object.values(filter);
+
+  let query: string;
+  if (keys.length === 0) {
+    query = `SELECT * FROM ${table}`;
+  } else {
+    const placeholders = values.map((v, i) => `${v}=$${i + 1}`).join(", ");
+    query = `SELECT * FROM ${table} WHERE ${placeholders}`;
+  }
+
+  return pool.query<T[]>(query, values).then((q) => q.rows);
+};
+
+export const updateObject = async <T>(
+  table: ITableNames,
+  data: Partial<T>
+) => {};
 
 export const initDB = async () => {
   await Promise.all([
@@ -33,5 +54,6 @@ export const initDB = async () => {
     pool.query(COMMENT_SCHEMA),
     pool.query(POST_SCHEMA),
     pool.query(USER_SCHEMA),
+    pool.query(UUID_EXTENSION),
   ]);
 };
